504d1072389fd795010498d738275a8c3e4f8313
sableHoujuu <dktpoole3@gmail.com>
2026-01-01 08:05:14 -0500

    added framerate limit and deltatime variable

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:  # game loop
         log_state()
 
         for event in pygame.event.get():
@@ -19,6 +21,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
